35fd2e3fe7dfe6eaad1259cc63e8bd99d474ef56
DESKTOP-GKG54A0\Pi <yepei14@outlook.com>
2017-07-25 21:54:42 +0800

    1、修改很多bug 2、修改计算needToRepairAll的算法错误 3、在xxxRepair.py的辅助线程中存储文件，增加安全性

--- autoRepair.py
+++ autoRepair.py
@@ -1,7 +1,6 @@
 import os
 import re
 import sys
-import threading
 
 class virtualNode:
     def __init__(self, ip, token):
@@ -9,107 +8,89 @@ class virtualNode:
         self.token = token
 
 def getTokenInText(text):
-    return re.findall(r" (-?\d+)", text)[-1]
+    return re.findall(r"(-?\d+)", text)[-1]
 
 def getIpInText(text):
     return re.findall(r"(\d+\.\d+\.\d+\.\d+)", text)[0]
 
-text = os.popen("./nodetool ring").readlines()
-# text = open("tokens").readlines()
+# Get all ip address
+text = os.popen("nodetool status").readlines()
+ipToRange = dict()
+for i in range(5, len(text) - 1):
+    if "DN" in text[i]:
+        continue
+    ipToRange[getIpInText(text[i])] = set()
+
+text = os.popen("nodetool ring").readlines()
 keyspaceAndCF = sys.argv[1]
 numOfReplicas = sys.argv[2]
 needToRepairOri = set()
 needToRepairAll = set()
+localRange = set()
 deadNode = set()
 virtualNodes = []
-
-fp = open(keyspaceAndCF + ".txt", 'w')
-fp.writelines(keyspaceAndCF + "\n")
 
 # Store all virtual nodes and original ranges that need to be repaired
 print "Process 1 : Storing all virtual nodes and original ranges that need to be repaired"
 for i in range(5, len(text) - 6):
-    virtualNodes.append(virtualNode(getIpInText(text[i]), getTokenInText(text[i])))
+    ip = getIpInText(text[i])
+    virtualNodes.append(virtualNode(ip, getTokenInText(text[i])))
     if 'Down' in text[i]:
-        deadNode.add(getIpInText(text[i]))
+        deadNode.add(ip)
         needToRepairOri.add(i - 5)
+    else:
+        ipToRange[ip].add(i - 5)
 print "Process 1 finished"
 
 if len(needToRepairOri) == 0:
     print "All nodes are alive"
     sys.exit(0)
 
+for i in needToRepairOri:
+    j = i + 1 if i + 1 < len(virtualNodes) else 0
+    while virtualNodes[j].ip in deadNode:
+        j = j + 1 if j + 1 < len(virtualNodes) else 0
+    ipToRange[virtualNodes[j].ip].add(i)
+
 # Find all ranges that need to be repaired
 print "Process 2 : Searching for all ranges that need to be repaired"
 for i in needToRepairOri:
     ip1 = virtualNodes[i].ip
-    replicaDict = {ip1 : i}
+    replicaIp = {ip1}
     j = len(virtualNodes) - 1 if i - 1 < 0 else i - 1
-    while j != i and len(replicaDict) < numOfReplicas:
+    while j != i:
         ip2 = virtualNodes[j].ip
         if ip2 == ip1:
             break
+        elif ip2 in replicaIp or len(replicaIp) < numOfReplicas:
+            replicaIp.add(ip2)
+            needToRepairAll.add(j)
         else:
-            replicaDict[ip2] = j
+            break
         j = len(virtualNodes) - 1 if j - 1 < 0 else j - 1
     if i == j:
         print "Warn: The number of nodes is less than the num of replicas!"
-    for value in replicaDict.values():
-        needToRepairAll.add(value)
-needToRepairAll = needToRepairAll | needToRepairOri
 print "Process 2 finished"
 
-numOfRanges = len(needToRepairAll)
-for i in needToRepairAll:
-    j = len(virtualNodes) - 1 if i - 1 < 0 else i - 1
-    fp.write(virtualNodes[j].token + " " + virtualNodes[i].token + "\n")
+for ip in ipToRange:
+    fp = open(keyspaceAndCF + "_" + ip.replace(".", "_") + ".txt", 'w')
+    fp.writelines(keyspaceAndCF + "\n")
+    ipToRange[ip] = ipToRange[ip] & needToRepairAll
+    numOfRanges = len(ipToRange[ip])
+    for i in ipToRange[ip]:
+        j = len(virtualNodes) - 1 if i - 1 < 0 else i - 1
+        fp.write(virtualNodes[j].token + " " + virtualNodes[i].token + "\n")
+    fp.writelines('0 ranges have been repaired' + '\n')
+    fp.close()
 
 # Assassinate dead nodes
 print "Process 3 : Assassinating dead nodes"
 for i in deadNode:
     print "Process 3 : Assassinating " + i
-    os.system("./nodetool assassinate %s" % i)
+    os.system("nodetool assassinate %s" % i)
 print "Process 3 finished"
 
-terminateProcess = False
-def terminateProcessManually():
-    print raw_input()
-    global terminateProcess
-    terminateProcess = True
-terminateThread = threading.Thread(target=terminateProcessManually)
-terminateThread.start()
-
-print "Process 4: Repairing all ranges that need to be repaired"
-hasFailure = False
-numOfRepaired = 0
-fpFailed = open(keyspaceAndCF + "_failed.txt", "w")
-fpFailed.write(keyspaceAndCF + '\n')
-# Repair all ranges that need to be repaired
-for i in needToRepairAll:
-    if terminateProcess == True:
-        print "Process 4 is terminated manually"
-        break
-    numOfRepaired = numOfRepaired + 1
-    j = len(virtualNodes) - 1 if i - 1 < 0 else i - 1
-    log = os.popen("./nodetool repair -st %s -et %s %s" %\
-          (virtualNodes[j].token, virtualNodes[i].token, keyspaceAndCF)).read()
-    if "successfully" in log:
-        print str(numOfRepaired) + " / " + str(numOfRanges) + " has been repaired successfully"
-    else:
-        hasFailure = True
-        print "Failed to repair range " + str(numOfRepaired)
-        print log
-        fpFailed.write(virtualNodes[j].token + " " + virtualNodes[i].token + "\n")
-    print "Press Enter to terminate the process"
-print "Process 4 finished"
-
-if hasFailure:
-    print str(numOfRepaired) + ' ranges have been repaired with some failures'
-    fpFailed.writelines(0 + ' ranges have been repaired' + '\n')
-    fpFailed.close()
-else:
-    print str(numOfRepaired) + ' ranges have been repaired'
-    fpFailed.close()
-    os.remove(keyspaceAndCF + "_failed.txt")
-fp.writelines(str(numOfRepaired) + ' ranges have been repaired' + '\n')
-fp.close()
+print "autoRepair : Starting xxxRepair"
+for ip in ipToRange:
+    sys.argv = ["xxxRepair.py", ip, keyspaceAndCF + "_" + ip.replace(".", "_") + ".txt"]
+    execfile("xxxRepair.py")
--- xxxRepair.py
+++ xxxRepair.py
@@ -6,7 +6,8 @@ import threading
 def getNumbersInText(text):
     return re.findall(r"(-?\d+)", text)
 
-fp = open(sys.argv[1], 'r')
+fp = open(sys.argv[2], 'r')
+ip = sys.argv[1]
 text = fp.read()
 fp.close()
 logNum = text.count('ranges have been repaired')
@@ -15,25 +16,37 @@ totalRangeNum = len(text) - logNum - 2
 keyspaceAndCF = text[0]
 repairedNum = int(getNumbersInText(text[-2])[0])
 
+print "xxxRepair : Repairing all ranges that need to be repaired"
+hasFailure = False
+fpFailed = open(sys.argv[2][:-4] + "_failed.txt", "w")
+fpFailed.write(keyspaceAndCF + '\n')
+
 terminateProcess = False
 def terminateProcessManually():
     print raw_input()
-    print "Repair is terminated manually"
     global terminateProcess
     terminateProcess = True
+    if hasFailure:
+        fpFailed.writelines('0 ranges have been repaired' + '\n')
+        fpFailed.close()
+    else:
+        fpFailed.close()
+        os.remove(sys.argv[2][:-4] + "_failed.txt")
+    fp = open(sys.argv[2], 'a')
+    fp.writelines(str(repairedNum) + ' ranges have been repaired' + '\n')
+    fp.close()
 terminateThread = threading.Thread(target=terminateProcessManually)
 terminateThread.start()
 
-hasFailure = False
-fpFailed = open(sys.argv[1][:-4] + "_failed.txt", "w")
-fpFailed.write(keyspaceAndCF + '\n')
 # Repair all ranges that need to be repaired
 for i in range(repairedNum + 1, totalRangeNum + 1):
     if terminateProcess == True:
+        print "xxxRepair is terminated manually"
         break
+    print "Repairing range " + str(repairedNum + 1) + " ..."
+    log = os.popen("nodetool -h %s repair -st %s -et %s %s" % (ip,\
+            getNumbersInText(text[i])[0], getNumbersInText(text[i])[1], keyspaceAndCF)).read()
     repairedNum = repairedNum + 1
-    log = os.popen("./nodetool repair -st %s -et %s %s" % \
-                   (getNumbersInText(text[i])[0], getNumbersInText(text[i])[1], keyspaceAndCF)).read()
     if "successfully" in log:
         print str(repairedNum) + " / " + str(totalRangeNum) + " has been repaired successfully"
     else:
@@ -45,12 +58,5 @@ for i in range(repairedNum + 1, totalRangeNum + 1):
 
 if hasFailure:
     print str(repairedNum) + ' ranges have been repaired with some failures'
-    fpFailed.writelines(0 + ' ranges have been repaired' + '\n')
-    fpFailed.close()
 else:
     print str(repairedNum) + ' ranges have been repaired'
-    fpFailed.close()
-    os.remove(sys.argv[1][:-4] + "_failed.txt")
-fp = open(sys.argv[1], 'a')
-fp.writelines(str(repairedNum) + ' ranges have been repaired' + '\n')
-fp.close()
